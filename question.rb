580c86d6803414455318e91b0e28fa7b7bd37882
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-30 02:51:23 +0000

    change random number range to 20

--- question.rb
+++ question.rb
@@ -6,8 +6,8 @@ class Question
   attr_reader :num1, :num2
 
   def initialize
-    @num1 = rand(1..10)
-    @num2 = rand(1..10)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
   end
 
   def player_guess
